78f3baf339ad17f06b782ef16a69ac553a03bb26
Gasol Wu <gasol.wu@gmail.com>
2013-08-22 19:34:30 +0800

    Use gettext

--- sysutils/py-glances/files/patch-setup.py
+++ sysutils/py-glances/files/patch-setup.py
@@ -1,5 +1,5 @@
 --- ./setup.py.orig	2013-08-20 19:49:41.000000000 +0800
-+++ ./setup.py	2013-08-22 18:39:18.000000000 +0800
++++ ./setup.py	2013-08-22 19:27:24.000000000 +0800
 @@ -11,10 +11,9 @@
                             'docs/glances-doc.html',
                             'glances/conf/glances.conf']),
@@ -14,11 +14,15 @@
  ]
  
  if hasattr(sys, 'real_prefix') or 'bsd' in sys.platform:
-@@ -23,7 +22,6 @@
+@@ -23,10 +22,9 @@
      etc_path = os.path.join('/etc', 'glances')
  elif 'darwin' in sys.platform:
      etc_path = os.path.join('/usr/local', 'etc', 'glances')
 -data_files.append((etc_path, ['glances/conf/glances.conf']))
  
  for mo in glob.glob('i18n/*/LC_MESSAGES/*.mo'):
-     data_files.append((os.path.dirname(mo).replace('i18n/', 'share/locale/'), [mo]))
+-    data_files.append((os.path.dirname(mo).replace('i18n/', 'share/locale/'), [mo]))
++    data_files.append((os.path.dirname(mo), [mo]))
+ 
+ setup(
+     name='Glances',
