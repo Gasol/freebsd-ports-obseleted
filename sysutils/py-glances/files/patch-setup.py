4a87ab327db61a70512dde2075a59f2b19b6f7b0
Gasol Wu <gasol.wu@gmail.com>
2013-12-15 22:35:18 +0800

    Update sysutils/py-glances to 1.7.3

--- sysutils/py-glances/files/patch-setup.py
+++ sysutils/py-glances/files/patch-setup.py
@@ -1,24 +1,10 @@
---- ./setup.py.orig	2013-11-01 17:12:25.000000000 +0800
-+++ ./setup.py	2013-11-08 14:42:57.000000000 +0800
-@@ -11,10 +11,9 @@
-                            'docs/glances-doc.html',
-                            'glances/conf/glances.conf']),
-     ('share/doc/glances/images', glob.glob('docs/images/*.png')),
--    ('share/glances/css', glob.glob('glances/data/css/*.css')),
--    ('share/glances/html', glob.glob('glances/data/html/*.html')),
--    ('share/glances/img', glob.glob('glances/data/img/*.png')),
--    ('share/man/man1', ['docs/man/glances.1'])
-+    ('glances/data/css', glob.glob('glances/data/css/*.css')),
-+    ('glances/data/html', glob.glob('glances/data/html/*.html')),
-+    ('glances/data/img', glob.glob('glances/data/img/*.png')),
- ]
- 
- if hasattr(sys, 'real_prefix') or 'bsd' in sys.platform:
-@@ -25,10 +24,6 @@
+--- ./setup.py.orig	2013-12-15 17:23:06.000000000 +0800
++++ ./setup.py	2013-12-15 22:25:56.000000000 +0800
+@@ -21,10 +21,6 @@
      conf_path = os.path.join('/usr/local', 'etc', 'glances')
  elif 'win32' in sys.platform:
      conf_path = os.path.join(os.environ.get('APPDATA'), 'glances')
--data_files.append((conf_path, ['glances/conf/glances.conf']))
+-data_files.append((conf_path, ['conf/glances.conf']))
 -
 -for mo in glob.glob('i18n/*/LC_MESSAGES/*.mo'):
 -    data_files.append((os.path.dirname(mo).replace('i18n/', 'share/locale/'), [mo]))
