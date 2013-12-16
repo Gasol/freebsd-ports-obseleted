e4f8b2af6f72a65a157e123c7068143cf9423079
Gasol Wu <gasol.wu@gmail.com>
2013-12-16 21:47:22 +0800

    Remove non-gzipped man file glances.1

--- sysutils/py-glances/files/patch-setup.py
+++ sysutils/py-glances/files/patch-setup.py
@@ -1,6 +1,16 @@
 --- ./setup.py.orig	2013-12-15 17:23:06.000000000 +0800
-+++ ./setup.py	2013-12-15 22:25:56.000000000 +0800
-@@ -21,10 +21,6 @@
++++ ./setup.py	2013-12-16 21:45:25.000000000 +0800
+@@ -9,8 +9,7 @@
+ data_files = [
+     ('share/doc/glances', ['AUTHORS', 'COPYING', 'NEWS', 'README.rst',
+                            'conf/glances.conf', 'docs/glances-doc.html']),
+-    ('share/doc/glances/images', glob.glob('docs/images/*.png')),
+-    ('share/man/man1', ['man/glances.1'])
++    ('share/doc/glances/images', glob.glob('docs/images/*.png'))
+ ]
+ 
+ if hasattr(sys, 'real_prefix') or 'bsd' in sys.platform:
+@@ -21,10 +20,6 @@
      conf_path = os.path.join('/usr/local', 'etc', 'glances')
  elif 'win32' in sys.platform:
      conf_path = os.path.join(os.environ.get('APPDATA'), 'glances')
