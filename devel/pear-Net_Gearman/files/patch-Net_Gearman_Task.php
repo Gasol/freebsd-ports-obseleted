6f0ef84d13c75d44d4f9c4842a1cec81c41e94a9
Gasol Wu <gasol.wu@gmail.com>
2015-05-26 04:18:28 +0800

    Update devel/pear-Net_Gearman to 0.2.3_1
    Fix constant JOB_HIGH that share same value with JOB_NORMAL

---
+++ devel/pear-Net_Gearman/files/patch-Net_Gearman_Task.php
@@ -0,0 +1,11 @@
+--- Net/Gearman/Task.php.orig	2015-05-25 20:13:57 UTC
++++ Net/Gearman/Task.php
+@@ -144,7 +144,7 @@ class Net_Gearman_Task
+      *
+      * @var integer JOB_HIGH
+      */
+-    const JOB_HIGH = 2;
++    const JOB_HIGH = 3;
+ 
+     /**
+      * Callback of type complete
